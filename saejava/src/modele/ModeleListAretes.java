6ceefe44fe865a2d7de526f75ff2411ee745dc23
FrancoisGraux <frsgra28@gmail.com>
2022-06-13 19:53:07 +0200

    Commentaire Javadoc modele

--- saejava/src/modele/ModeleListAretes.java
+++ saejava/src/modele/ModeleListAretes.java
@@ -6,24 +6,39 @@ import javax.swing.AbstractListModel;
 import metier.Arete;
 
 /**
- *
- * @author François, Elliot
+ * La classe pour crée un model de liste d'arête
+ * @author François Graux
+ * @author Elliot Curvat
+ * @version JDK 11.0.13
  */
 public class ModeleListAretes extends AbstractListModel<String> {
 
 	private final List<Arete> dataAutouroute = new ArrayList<>();
 	
+	/**
+	 * Getteur de la taille
+	 * @return revoie la taille du modele
+	 */
 	@Override
 	public int getSize() {
 		return dataAutouroute.size();
 	}
-
+	
+	/**
+	 * Getteur pour un element par sont indice
+	 * @param index indice de l'element
+	 * @return la valeur trouver ou null si pas de valeur
+	 */
 	@Override
 	public String getElementAt(int index) {
 		String val = dataAutouroute.get(index).getType()+dataAutouroute.get(index).getLongueur();
 		return val;
 	}
 	
+	/**
+	 * Ajoute les arêtes au modéle
+	 * @param listeAutoroute une liste d'arête
+	 */
 	public void ajouterAretes(List<Arete> listeAutoroute){
 		for(Arete elem:listeAutoroute){
 			dataAutouroute.add(elem);
@@ -31,6 +46,10 @@ public class ModeleListAretes extends AbstractListModel<String> {
 		fireIntervalAdded(this, 0, dataAutouroute.size());
 		
 	}
+	
+	/**
+	 * vide le modéle
+	 */
 	public void viderModele(){
 		dataAutouroute.clear();
 	}
--- saejava/src/modele/ModeleListSommets.java
+++ saejava/src/modele/ModeleListSommets.java
@@ -6,23 +6,38 @@ import javax.swing.AbstractListModel;
 import metier.Sommet;
 
 /**
- *
- * @author François, Elliot
+ * La classe pour crée un model de liste d'arête
+ * @author François Graux
+ * @author Elliot Curvat
+ * @version JDK 11.0.13
  */
 public class ModeleListSommets extends AbstractListModel<String>{
 	
 	private final List<Sommet> dataVille =new ArrayList<>();
-
+	
+	/**
+	 * Getteur de la taille
+	 * @return revoie la taille du modele
+	 */
 	@Override
 	public int getSize() {
 		return dataVille.size();
 	}
-
+	
+	/**
+	 * Getteur pour un element par sont indice
+	 * @param index indice de l'element
+	 * @return la valeur trouver ou null si pas de valeur
+	 */
 	@Override
 	public String getElementAt(int index) {
 		return dataVille.get(index).getNom();
 	}
 	
+	/**
+	 * Ajoute les sommets au modéle
+	 * @param listeVille une liste de sommet
+	 */
 	public void ajouterSommets(List<Sommet> listeVille){
 		for(Sommet elem:listeVille){
 			dataVille.add(elem);
@@ -30,6 +45,9 @@ public class ModeleListSommets extends AbstractListModel<String>{
 		fireIntervalAdded(this, 0, dataVille.size());	
 	}
 	
+	/**
+	 * vide le modéle
+	 */
 	public void viderModele(){
 		dataVille.clear();
 	}
